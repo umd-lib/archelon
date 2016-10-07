6453fd2b6b5e6be24af5c0af4e201cd0d9a95f42
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-10-07 11:44:48 -0400

    LIBHYDRA-39. Added mirador view fragment and supporting logic.
    https://issues.umd.edu/browse/LIBHYDRA-39

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  FEDORA_BASE_URL = "https://fcrepolocal/fcrepo/rest/tmp/"
+  IIIF_URL_PREFIX = "https://iiiflocal/images/"
+  IIIF_URL_SUFFIX = "/full/full/0/default.jpg"
+  FEDORA_BINARY = "http://fedora.info/definitions/v4/repository#Binary"
+  ALLOWED_MIME_TYPES = ["image/jpeg", "image/tiff", "image/jp2"]
+
+  def is_mirador_displayable(document)
+    document._source[:rdf_type].include? FEDORA_BINARY and ALLOWED_MIME_TYPES.include? document._source[:mime_type]
+  end
+
+  def mirador_link(id)
+    puts id
+    sliced = id.slice!(FEDORA_BASE_URL)
+    puts sliced
+    puts id
+    iiif_url = IIIF_URL_PREFIX + id + IIIF_URL_SUFFIX
+  end
 end
