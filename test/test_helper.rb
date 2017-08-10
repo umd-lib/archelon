0fa00d9cf84c37ae27dc644aa4e580576568b11c
David P. Steelman <dsteelma@umd.edu>
2017-08-10 09:29:30 -0400

    LIBHYDRA-105 Added "minitest/reporters' to test_helper.rb
    Added to make more consistent with other projects.
    
    https://issues.umd.edu/browse/LIBHYDRA-105

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require 'rails/test_help'
 # Require minitest Mock functionality
 require 'minitest/autorun'
 
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
