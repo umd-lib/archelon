b290375738b013b1a27e65eb8ac77a995941f574
Peter Eichman <peichman@umd.edu>
2017-12-08 11:11:38 -0500

    LIBHYDRA-113. SSL configuration for downloads.
    Default to doing SSL peer verification of certificates, but disable it for the Vagrant environment, since fcrepolocal uses a self-signed certificate. Also corrected a mismatched tag in the retrieve view.
    
    https://issues.umd.edu/browse/LIBHYDRA-113

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module FcrepoSearch
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Default to doing SSL certificate verification
+    config.fcrepo_ssl_verify_mode = OpenSSL::SSL::VERIFY_PEER
   end
 end
