eaa2565cd5842a554c8b0178a217cd5b34dfe4d3
Mohamed Abdul Rasheed <mohideen@umd.edu>
2019-06-12 12:50:51 -0400

    LIBHYDRA-161. Fix login path.
    https://issues.umd.edu/browse/LIBHYDRA-161

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   get path: '/retrieve/do/:token', controller: 'retrieve', action: 'do_retrieve',
       as: 'do_retrieve'
 
-  get 'login', to: redirect('/auth/:provider'), as: 'login'
+  get 'login', to: redirect('/auth/cas'), as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
