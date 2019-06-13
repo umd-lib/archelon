4e10cb743b7604dbc7b13d47f0ad8e8a9705818b
Mohamed Abdul Rasheed <mohideen@umd.edu>
2019-06-13 12:07:05 -0400

    LIBHYDRA-155. Updated tests.
    - Removed tests for CasUser edit feature.
    - Added tests for Grouper based user behavior.
    
    https://issues.umd.edu/browse/LIBHYDRA-155

--- test/controllers/cas_users_controller_test.rb
+++ test/controllers/cas_users_controller_test.rb
@@ -12,32 +12,9 @@ class CasUsersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:cas_users)
   end
 
-  test 'should get new' do
-    get :new
-    assert_response :success
-  end
-
-  test 'should create cas_user' do
-    assert_difference('CasUser.count') do
-      post :create, cas_user: { cas_directory_id: 'newuser', name: 'New User' }
-    end
-
-    assert_redirected_to cas_user_path(assigns(:cas_user))
-  end
-
   test 'should show cas_user' do
     get :show, id: @cas_user
     assert_response :success
-  end
-
-  test 'should get edit' do
-    get :edit, id: @cas_user
-    assert_response :success
-  end
-
-  test 'should update cas_user' do
-    patch :update, id: @cas_user, cas_user: { cas_directory_id: @cas_user.cas_directory_id, name: @cas_user.name }
-    assert_redirected_to cas_user_path(assigns(:cas_user))
   end
 
   test 'should destroy cas_user' do
@@ -51,18 +28,6 @@ class CasUsersControllerTest < ActionController::TestCase
   test 'non-admin users should not have access to index, new, create, edit, update,destroy' do
     run_as_user(cas_users(:one)) do
       get :index
-      assert_response :forbidden
-
-      get :new
-      assert_response :forbidden
-
-      post :create, cas_user: { cas_directory_id: 'newuser', name: 'New User' }
-      assert_response :forbidden
-
-      get :edit, id: @cas_user
-      assert_response :forbidden
-
-      patch :update, id: @cas_user, cas_user: { cas_directory_id: @cas_user.cas_directory_id, name: @cas_user.name }
       assert_response :forbidden
 
       delete :destroy, id: @cas_user
--- test/integration/cas_authorization_test.rb
+++ test/integration/cas_authorization_test.rb
@@ -9,6 +9,80 @@ class CasAuthorizationTest < ActionDispatch::IntegrationTest
     assert_template 'catalog/index'
   end
 
+  test 'new cas_user with User Grouper group can access application' do
+    ldap_attrs = {
+      name: "New CAS User",
+      groups: [GROUPER_USER_GROUP]
+    }
+    CasUser.stub :ldap_attributes, ldap_attrs do
+      cas_login('new_cas_user1')
+    end
+    get root_path
+    assert_response :success
+    assert_template 'catalog/index'
+  end
+
+  test 'new cas_user can with User Grouper group has assigned "user" type' do
+    ldap_attrs = {
+      name: "New CAS User",
+      groups: [GROUPER_USER_GROUP]
+    }
+    CasUser.stub :ldap_attributes, ldap_attrs do
+      cas_login('new_cas_user2')
+    end
+    assert CasUser.find_by(cas_directory_id: 'new_cas_user2').user?
+  end
+
+  test 'new cas_user can with Admin Grouper group can access application' do
+    ldap_attrs = {
+      name: "New Admin CAS User",
+      groups: [GROUPER_ADMIN_GROUP]
+    }
+    CasUser.stub :ldap_attributes, ldap_attrs do
+      cas_login('new_admin_cas_user1')
+    end
+    
+    get root_path
+    assert_response :success
+    assert_template 'catalog/index'
+  end
+
+  test 'new cas_user can with Admin Grouper group has "admin" type' do
+    ldap_attrs = {
+      name: "New Admin CAS User",
+      groups: [GROUPER_ADMIN_GROUP]
+    }
+    CasUser.stub :ldap_attributes, ldap_attrs do
+      cas_login('new_admin_cas_user2')
+    end
+    assert CasUser.find_by(cas_directory_id: 'new_admin_cas_user2').admin?
+  end
+
+  test 'new cas_user without Grouper groups cannot access application' do
+    ldap_attrs = {
+      name: "Unauthorized User",
+      groups: ["SOME_OTHER_GROUP"]
+    }
+    CasUser.stub :ldap_attributes, ldap_attrs do
+      cas_login('new_unauth_user1')
+    end
+
+    get root_path
+    assert_response(:forbidden)
+  end
+
+  test 'new cas_user without Grouper groups has "unauthorized" type' do
+    ldap_attrs = {
+      name: "Unauthorized User",
+      groups: ["SOME_OTHER_GROUP"]
+    }
+    CasUser.stub :ldap_attributes, ldap_attrs do
+      cas_login('new_unauth_user2')
+    end
+  
+    assert CasUser.find_by(cas_directory_id: 'new_unauth_user2').unauthorized?
+  end
+
   test 'non-existent cas_user cannot access application' do
     cas_login('no_such_user')
 
