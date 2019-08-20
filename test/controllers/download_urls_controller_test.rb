f561fb6a2660c8b4f9181d7a2a89036e51b9536e
David P. Steelman <dsteelma@umd.edu>
2019-08-20 09:22:35 -0400

    LIBHYDRA-179. Fixed "Style/NumericPredicate" offenses
    Replaced numeric test with ".positive?" predicate.
    
    https://issues.umd.edu/browse/LIBHYDRA-179

--- test/controllers/download_urls_controller_test.rb
+++ test/controllers/download_urls_controller_test.rb
@@ -139,7 +139,7 @@ class DownloadUrlsControllerTest < ActionController::TestCase
     get :index, params: { rq: { enabled_eq: 1 } }
 
     download_urls = assigns(:download_urls)
-    assert download_urls.count > 0
+    assert download_urls.count.positive?
     assert download_urls.all?(&:enabled?)
   end
 
@@ -148,7 +148,7 @@ class DownloadUrlsControllerTest < ActionController::TestCase
     get :index, params: { rq: { creator_eq: creator } }
 
     download_urls = assigns(:download_urls)
-    assert download_urls.count > 0
+    assert download_urls.count.positive?
     assert(download_urls.all?) { |d| d.creator == creator }
   end
 
