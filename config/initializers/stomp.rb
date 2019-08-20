93dabc15506bad32aabc56639b8596317cb1c560
Peter Eichman <peichman@umd.edu>
2019-08-20 15:55:46 -0400

    LIBHYDRA-172. Only start Stomp connection when Rails server is actually running.
    https://issues.umd.edu/browse/LIBHYDRA-172

--- config/initializers/stomp.rb
+++ config/initializers/stomp.rb
@@ -1,1 +1,4 @@
-STOMP_CLIENT = Stomp::Client.new(hosts: [ Rails.configuration.stomp_server ])
+# Only start Stomp connection when Rails server is actually running
+if defined?(Rails::Server)
+  STOMP_CLIENT = Stomp::Client.new(hosts: [Rails.configuration.stomp_server])
+end
