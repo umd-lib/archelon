2c57de12e447f0237105df682f47f8a7897a043b
David P. Steelman <dsteelma@umd.edu>
2019-08-21 13:43:25 -0400

    LIBHYDRA-176. Export job controller to shows error when disconnected
    If the application is no longer connected to ActiveMQ, the
    export job controller sets the status of the job to "Error", and
    displays a flash error message to the user.
    
    https://issues.umd.edu/browse/LIBHYDRA-176

--- test/controllers/export_jobs_controller_test.rb
+++ test/controllers/export_jobs_controller_test.rb
@@ -29,5 +29,25 @@ class ExportJobsControllerTest < ActionController::TestCase
 
       post :create, params: params
     end
+
+    assert_redirected_to export_jobs_path
+  end
+
+  test 'creating new export job when not connected should raise error' do
+    expect(STOMP_CLIENT).to receive(:publish).and_raise(Stomp::Error::NoCurrentConnection.new)
+
+    assert_difference('ExportJob.count') do
+      uris = 'http://example.com/1\nhttp://example.com/2'
+      params = {}
+      params[:uris] = uris
+      params[:export_job] = { name: 'test1', format: 'CSV' }
+
+      post :create, params: params
+    end
+    export_job = ExportJob.last
+    assert_equal 'Error', export_job.status
+
+    assert_equal I18n.t(:active_mq_is_down), flash[:error]
+    assert_redirected_to export_jobs_path
   end
 end
