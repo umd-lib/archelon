80d426c9e747eeeef2baff2bbcba6e09ad891758
David P. Steelman <dsteelma@umd.edu>
2019-08-22 15:29:22 -0400

    LIBHYDRA-184. Added Rails version number to migrations
    Determined which Rails version each migration file was created under,
    and added to the migration.
    
    https://issues.umd.edu/browse/LIBHYDRA-184

--- db/migrate/20190612201137_remove_admin_from_cas_user.rb
+++ db/migrate/20190612201137_remove_admin_from_cas_user.rb
@@ -1,4 +1,4 @@
-class RemoveAdminFromCasUser < ActiveRecord::Migration
+class RemoveAdminFromCasUser < ActiveRecord::Migration[4.2]
   def change
     remove_column :cas_users, :admin, :boolean
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190812194529) do
+ActiveRecord::Schema.define(version: 2019_08_12_194529) do
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer  "user_id",       null: false
-    t.string   "user_type"
-    t.string   "document_id"
-    t.string   "document_type"
-    t.binary   "title"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "user_id", null: false
+    t.string "user_type"
+    t.string "document_id"
+    t.string "document_type"
+    t.binary "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["document_id"], name: "index_bookmarks_on_document_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "cas_users", force: :cascade do |t|
-    t.string   "cas_directory_id"
-    t.string   "name"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "user_type"
+    t.string "cas_directory_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_type"
     t.index ["cas_directory_id"], name: "index_cas_users_on_cas_directory_id", unique: true
   end
 
   create_table "download_urls", force: :cascade do |t|
-    t.string   "token"
-    t.string   "url"
-    t.string   "title"
-    t.text     "notes"
-    t.string   "mime_type"
-    t.string   "creator"
-    t.boolean  "enabled"
-    t.string   "request_ip"
-    t.string   "request_user_agent"
+    t.string "token"
+    t.string "url"
+    t.string "title"
+    t.text "notes"
+    t.string "mime_type"
+    t.string "creator"
+    t.boolean "enabled"
+    t.string "request_ip"
+    t.string "request_user_agent"
     t.datetime "accessed_at"
     t.datetime "download_completed_at"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "expires_at"
     t.index ["token"], name: "index_download_urls_on_token", unique: true
   end
 
   create_table "export_jobs", force: :cascade do |t|
-    t.string   "format"
-    t.integer  "cas_user_id"
+    t.string "format"
+    t.integer "cas_user_id"
     t.datetime "timestamp"
-    t.string   "name"
-    t.integer  "item_count"
-    t.string   "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.integer "item_count"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["cas_user_id"], name: "index_export_jobs_on_cas_user_id"
   end
 
   create_table "searches", force: :cascade do |t|
-    t.binary   "query_params"
-    t.integer  "user_id"
-    t.string   "user_type"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.binary "query_params"
+    t.integer "user_id"
+    t.string "user_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
