d9d8534e761c3166e41437d869db7eed568601dd
David P. Steelman <dsteelma@umd.edu>
2019-08-22 08:11:06 -0400

    LIBHYDRA-176. Moved StompClient to app/services/
    Created app/services/ directory, and moved StompClient class into it,
    as that seemed to be a better place for the code.
    
    https://issues.umd.edu/browse/LIBHYDRA-176

--- app/services/stomp_client.rb
+++ app/services/stomp_client.rb
@@ -1,35 +1,31 @@
+# frozen_string_literal: true
+
+# Provides Stomp messaging, including handling subscription to queues
 class StompClient
+  # Returns the singleton instance
   def self.instance
-    @@instance ||= new
+    @@instance ||= new # rubocop:disable Style/ClassVars
   end
 
   private_class_method :new
 
+  # Initializes the client, and subscribes to queues
   def initialize
     @stomp_client = Stomp::Client.new(hosts: [Rails.configuration.stomp_server])
 
     @stomp_client.subscribe Rails.configuration.queues[:export_jobs_completed] do |stomp_msg|
       update_export_job(stomp_msg)
     end
-
-    # Faker to move export jobs to export jobs completed
-    @stomp_client.subscribe Rails.configuration.queues[:export_jobs] do |msg|
-      headers = msg.headers
-      archelon_headers = headers.delete_if { |key, _| !key.start_with?('Archelon') }
-      archelon_headers[:ArchelonExportJobStatus] = 'Ready'
-      body = msg.body
-      @stomp_client.publish Rails.configuration.queues[:export_jobs_completed], body, archelon_headers
-    end
   end
 
+  # Publishes the given message to the given destination with the given
+  # headers.
   def publish(destination, message, headers = {})
     @stomp_client.publish destination, message, headers
   end
 
+  # Updates ExportJob based on a Stomp message
   def update_export_job(stomp_msg)
-    puts "******** Stomp message ************"
-    puts "Msg: #{stomp_msg}"
-    puts "*******************************"
     headers = stomp_msg.headers
     export_job_id = headers['ArchelonExportJobId']
     export_job = ExportJob.find(export_job_id)
--- config/initializers/stomp.rb
+++ config/initializers/stomp.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-require './lib/utils/stomp_client'
-
 # Only start Stomp connection when Rails server is actually running
 STOMP_CLIENT = StompClient.instance if defined?(Rails::Server)
