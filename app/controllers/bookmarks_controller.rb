6af610962a5aecad66446f44a8ef7957bf3b3f54
Mohamed Abdul Rasheed <mohideen@umd.edu>
2019-08-26 18:07:05 -0400

    LIBHYDRA-183. Limit max selection to 1000.
    Changes to "Select all search results" button:
    - Disable if max selection reached
    - If all results cannot be selected, display a JS alert confirming if user want to proceed.
    
    https://issues.umd.edu/browse/LIBHYDRA-183

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,20 +19,27 @@ class BookmarksController < CatalogController
 
   def select_all_results # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     search_params = current_search_session.query_params
+    return redirect_back_to_catalog(params, search_params) unless current_user.bookmarks.count < 1000
     search_params['rows'] = params[:result_count]
     (@response, @document_list) = search_results(search_params)
     document_ids = @document_list.map(&:id)
     selected_ids = current_user.bookmarks.map(&:document_id)
     missing_ids = document_ids.reject { |doc_id| selected_ids.include?(doc_id) }
-    if missing_ids.length.zero?
+    select_ids = missing_ids.take(1000-current_user.bookmarks.count)
+    if select_ids.length.zero?
       flash[:notice] = I18n.t(:already_selected)
     else
-      missing_ids.each do |id|
+      select_ids.each do |id|
         bookmark = { document_id: id, document_type: blacklight_config.document_model.to_s }
         current_user.bookmarks.create(bookmark)
       end
-      flash[:notice] = I18n.t(:all_items_selected)
+      flash[:notice] = I18n.t(:items_selected, selected_count: select_ids.length)
     end
+    redirect_back_to_catalog(params, search_params)
+    end
+
+  private
+    def redirect_back_to_catalog(params, search_params)
     search_params[:per_page] = params[:per_page]
     search_params[:page] = params[:page]
     search_params.delete(:rows)
