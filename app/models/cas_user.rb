b7d5c0ef84bc772be06c480bc15fad5ce64bad16
Peter Eichman <peichman@umd.edu>
2020-02-14 13:29:54 -0500

    LIBHYDRA-211. Always check LDAP and update user info on login.
    https://issues.umd.edu/browse/LIBHYDRA-211

--- app/models/cas_user.rb
+++ app/models/cas_user.rb
@@ -2,7 +2,7 @@
 
 # A user of the application
 class CasUser < ApplicationRecord
-  # Connects this user object to Blacklights Bookmarks.
+  # Connects this user object to Blacklight's Bookmarks.
   include Blacklight::User
 
   enum user_type: { admin: 'admin', user: 'user', unauthorized: 'unauthorized' }
@@ -10,32 +10,19 @@ class CasUser < ApplicationRecord
   validates :name, presence: true
 
   def self.find_or_create_from_auth_hash(auth)
-    ldap_user_attributes = LdapUserAttributes.create(auth[:uid])
-    ldap_name = ldap_user_attributes.name
-    ldap_user_type = ldap_user_attributes.user_type
-
+    # update user info upon successful login, and return the user object
     where(cas_directory_id: auth[:uid]).first_or_initialize.tap do |user|
       user.cas_directory_id = auth[:uid]
-      update_name(user, ldap_name) unless user.name
-      update_user_type(user, ldap_user_type)
+      # only update from LDAP if we found anything
+      if user.ldap_attributes
+        user.name = user.ldap_attributes.name || user.cas_directory_id
+        user.user_type = user.ldap_attributes.user_type
+      end
       user.save!
     end
   end
 
-  # The following methods are not intended to be called from outside this class.
-  def self.update_name(user, name)
-    return if !name && user.name
-
-    user.name = user.cas_directory_id and return unless name # rubocop:disable Style/AndOr
-    user.name = name
-  end
-
-  def self.update_user_type(user, user_type)
-    return if !user_type && user.user_type
-
-    user.user_type = :unauthorized
-    return unless user_type
-
-    user.user_type = user_type
+  def ldap_attributes
+    @ldap_attributes ||= LdapUserAttributes.create(cas_directory_id)
   end
 end
