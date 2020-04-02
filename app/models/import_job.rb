766d2fa94fda3e3fa274b6139ddf2b59c3acf2fa
David P. Steelman <dsteelma@umd.edu>
2020-04-02 13:05:56 -0400

    LIBHYDRA-257. Updated ImportJob to delete PlastronOperations on delete
    Added "dependent:destroy" to PlastronOperation "belongs_to" to ensure
    that the related PlastronOperation record is removed when an ImportJob
    is deleted. This is consistent with the behavior in ExportJob.
    
    https://issues.umd.edu/browse/LIBHYDRA-257

--- app/models/import_job.rb
+++ app/models/import_job.rb
@@ -1,4 +1,4 @@
 class ImportJob < ApplicationRecord
   belongs_to :cas_user
-  belongs_to :plastron_operation
+  belongs_to :plastron_operation, dependent: :destroy
 end
--- test/models/import_job_test.rb
+++ test/models/import_job_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ImportJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "PlastronOperation is deleted when ImportJob is deleted" do
+    import_job = import_jobs(:one)
+    assert_not_nil(import_job.plastron_operation)
+
+    assert_difference('ImportJob.count', -1) do
+      assert_difference('PlastronOperation.count', -1) do
+        import_job.destroy!
+      end
+    end
+  end
+
+  test "ImportJob is not deleted when PlastronOperation is deleted" do
+    # Not sure if this is really the desired behavior. Ordinarily would
+    # expect a one-to-one relationship between ImportJob and it's
+    # associated PlastronOperation, so that deleting one deletes the other.
+    # Suspect it was done this way so that PlastronOperation would not
+    # need to deal with polymorphism between ExportJob/ImportJob in
+    # a "has_one" relationship.
+    plastron_op = plastron_operations(:import_op1)
+
+    assert_no_difference('ImportJob.count') do
+      assert_difference('PlastronOperation.count', -1) do
+        plastron_op.destroy!
+      end
+    end
+  end
 end
