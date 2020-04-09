4c0a7853653fb948280b21961a39b2f5c07e2968
David P. Steelman <dsteelma@umd.edu>
2020-04-09 11:16:52 -0400

    LIBHYDRA-257. Renamed database migration to be more descriptive
    https://issues.umd.edu/browse/LIBHYDRA-257

--- db/migrate/20200406155648_add_stage_to_import_jobs.rb
+++ db/migrate/20200406155648_add_stage_to_import_jobs.rb
@@ -1,4 +1,4 @@
-class AddStringToImportJobs < ActiveRecord::Migration[5.2]
+class AddStageToImportJobs < ActiveRecord::Migration[5.2]
   def change
     add_column :import_jobs, :stage, :string
   end
