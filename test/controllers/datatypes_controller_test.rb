ebc20cb1791736c6c691e8d379f8658d81fb05f2
David P. Steelman <dsteelma@umd.edu>
2020-04-30 11:12:11 -0400

    LIBHYDRA-285. Fixed failing tests
    Changed "DatatypesControllerTest" to an "ActionController::TestCase",
    and fixed all failing tests.
    
    Deleted "test/system/datatypes_test.rb", as we have not provided system
    tests for other models.
    
    https://issues.umd.edu/browse/LIBHYDRA-285

--- test/controllers/datatypes_controller_test.rb
+++ test/controllers/datatypes_controller_test.rb
@@ -1,46 +1,53 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class DatatypesControllerTest < ActionDispatch::IntegrationTest
+class DatatypesControllerTest < ActionController::TestCase
   setup do
+    @cas_user = cas_users(:test_admin)
+    mock_cas_login(@cas_user.cas_directory_id)
+
     @datatype = datatypes(:one)
   end
 
-  test "should get index" do
-    get datatypes_url
+  test 'should get index' do
+    get :index
     assert_response :success
   end
 
-  test "should get new" do
-    get new_datatype_url
+  test 'should get new' do
+    get :new
     assert_response :success
   end
 
-  test "should create datatype" do
+  test 'should create datatype' do
+    vocabulary = vocabularies(:vocab_one)
+
     assert_difference('Datatype.count') do
-      post datatypes_url, params: { datatype: { identifier: @datatype.identifier, vocabulary_id: @datatype.vocabulary_id } }
+      post :create, params: { datatype: { identifier: 'datatype_abc123', vocabulary_id: vocabulary.id } }
     end
 
     assert_redirected_to datatype_url(Datatype.last)
   end
 
-  test "should show datatype" do
-    get datatype_url(@datatype)
+  test 'should show datatype' do
+    get :show, params: { id: @datatype.id }
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_datatype_url(@datatype)
+  test 'should get edit' do
+    get :edit, params: { id: @datatype.id }
     assert_response :success
   end
 
-  test "should update datatype" do
-    patch datatype_url(@datatype), params: { datatype: { identifier: @datatype.identifier, vocabulary_id: @datatype.vocabulary_id } }
+  test 'should update datatype' do
+    patch :update, params: { id: @datatype.id, datatype: { indentifier: 'updated identifier' } }
     assert_redirected_to datatype_url(@datatype)
   end
 
-  test "should destroy datatype" do
+  test 'should destroy datatype' do
     assert_difference('Datatype.count', -1) do
-      delete datatype_url(@datatype)
+      delete :destroy, params: { id: @datatype.id }
     end
 
     assert_redirected_to datatypes_url
