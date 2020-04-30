d8bfc857148483bab019d4dbe9d497069cbb3f21
David P. Steelman <dsteelma@umd.edu>
2020-04-30 11:05:54 -0400

    LIBHYDRA-285. Added Datatype identifier uniqueness constraint
    Added "identifier" uniqueness constraint to "Datatype" model (the
    identifier must be unique within the vocabulary).
    
    Modeled after uniqueness constraint for the "identifier" field in the
    "Type" class, except that there is no requirement that the first letter
    be upper-case.
    
    https://issues.umd.edu/browse/LIBHYDRA-285

--- app/models/datatype.rb
+++ app/models/datatype.rb
@@ -1,3 +1,12 @@
 class Datatype < ApplicationRecord
+  validates :identifier,
+            presence: true,
+            format: { with: /\A[a-zA-Z0-9][a-zA-Z0-9_-]*\z/ },
+            uniqueness: {
+              scope: :vocabulary,
+              message: lambda do |object, data|
+                "\"#{data[:value]}\" is already used in the #{object.vocabulary.identifier} vocabulary"
+              end
+            }
   belongs_to :vocabulary
 end
