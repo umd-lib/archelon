f9d7f3f9ab8f67afe7d233d9abfbf9aa74f65381
Peter Eichman <peichman@umd.edu>
2020-05-01 09:49:34 -0400

    LIBHYDRA-287. Added datatype vocabulary to development and test seeds.
    - Updated the test seeds to just use the same ones as development.
    - Added a count of datatypes per vocabulary to the vocabulary index page.
    
    https://issues.umd.edu/browse/LIBHYDRA-287

--- db/seeds/test.rb
+++ db/seeds/test.rb
@@ -3,8 +3,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-# create a standard access vocabulary
-access_vocab = Vocabulary.find_or_create_by identifier: 'access'
-
-Type.create identifier: 'Public', vocabulary: access_vocab
-Type.create identifier: 'Campus', vocabulary: access_vocab
+# use the same seeds in test as development
+require Rails.root.join('db', 'seeds', 'development.rb')
