cf765bbdf8bc4a07e740e0fd156bc1e8746388f5
David P. Steelman <dsteelma@umd.edu>
2020-07-02 15:11:33 -0400

    LIBHYDRA-344. Initial implementation of "URIRef" React component
    Added "URIRef" component, and added to "Repeatable", so that a
    repeatable version would also be available.
    
    Added examples to React Stylistguide documentation, and to the
    http://localhost:3000/react_components page.
    
    https://issues.umd.edu/browse/LIBHYDRA-344

---
+++ app/javascript/components/URIRef.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import PropTypes, { string } from "prop-types"
+
+/**
+ * Input component consisting of a simple textbox.
+ *
+ *  Sample Rails view usage:
+ *
+ * ```
+ * <%= react_component(:URIRef, { paramPrefix: 'example', name: 'title', value: { "@id": "http://example.com/vocab#bar"} }) %>
+ * ```
+ *
+ * When used in a form, this will submit the array `example[title][]`
+ * with a single value, `{value: "http://example.com/vocab#bar"}`.
+ */
+class URIRef extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(event) {
+    this.setState({value: { "@id": event.target.value} })
+  }
+
+  render () {
+    let textbox_name = `${this.props.paramPrefix}[${this.props.name}][][value]`
+
+    let value = "";
+    if ((this.state.value) && (this.state.value["@id"])) {
+      value = this.state.value["@id"];
+    }
+
+    return (
+      <React.Fragment>
+        <input title={this.state.datatype} name={textbox_name} value={value} onChange={this.handleTextChange} size="40"/>
+      </React.Fragment>
+    );
+  }
+}
+
+URIRef.propTypes = {
+  /**
+   * The name of the element, used to with `paramPrefix` to construct the
+   * parameter sent via the form submission.
+   */
+  name: PropTypes.string,
+  /**
+   * Combined with the name (`<paramPrefix>[<name>][]`) to construct the
+   * parameter sent via the form submission.
+   */
+  paramPrefix: PropTypes.string,
+  /**
+   * The default text for the textbox
+   */
+  value: PropTypes.shape({
+    "@id": string
+  })
+}
+
+export default URIRef;
