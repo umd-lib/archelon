ecbd401d7aed29606546fae2d020c791201d1619
David P. Steelman <dsteelma@umd.edu>
2020-07-22 11:20:57 -0400

    LIBHYDRA-318. Updated form to indicate submission is in progress
    Added JavaScript so that when the "Submit" button is clicked, the button
    label will change to "Submitting" and become disabled. The button will
    be reenabled if a validation or other error occurs, so allow the
    user to resubmit the form.
    
    https://issues.umd.edu/browse/LIBHYDRA-318

--- app/assets/javascripts/resource.js
+++ app/assets/javascripts/resource.js
@@ -1,15 +1,44 @@
 // Handles AJAX response from "update" method in app/controllers/resource_controller.rb
 $(document).on('turbolinks:load', function() {
-  const element = document.querySelector("#resource_edit_form");
+
   const errorDiv = document.querySelector("#error_explanation");
   if (errorDiv) {
     errorDiv.innerHTML = "";
   }
 
+  const resourceFormSubmit = document.querySelector("#resource_form_submit");
+
+  function enableSubmitButton() {
+    if (resourceFormSubmit) {
+      resourceFormSubmit.disabled = false;
+      resourceFormSubmit.value = "Submit"
+    }
+  }
+
+  function disableSubmitButton() {
+    if (resourceFormSubmit) {
+      resourceFormSubmit.disabled = true;
+      resourceFormSubmit.value = "Submitting...";
+    }
+  }
+
+  // Ensure submit button is enabled on form load
+  enableSubmitButton();
+
+
+  const resourceForm = document.querySelector("#resource_edit_form");
+  if (resourceForm) {
+    resourceForm.addEventListener("submit", (event) => {
+      disableSubmitButton()
+    });
+  }
+
+  const element = document.querySelector("#resource_edit_form");
   if (element) {
     element.addEventListener("ajax:success", (event) => {
       const [data, _status, xhr] = event.detail;
 
+      enableSubmitButton();
       // ajax.success occurs even if a validation error occurrs, so
       // check for any errors
 
@@ -27,6 +56,7 @@ $(document).on('turbolinks:load', function() {
       }
     });
     element.addEventListener("ajax:error", () => {
+      enableSubmitButton();
       const [data, _status, xhr] = event.detail;
       errors = data.errors
       let alertText = errors.join("\n");
