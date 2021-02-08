4bcd7cfabd9f52ac3615bf5c3f03d7b410284d03
David P. Steelman <dsteelma@umd.edu>
2021-02-08 11:26:30 -0500

    LIBHYDRA-410. Refactored import request message into separate class
    Refactored "message_headers" out of the "ImportJobsController" class
    into a separate "ImportJobRequest" class, renaming to "headers". This
    simplifies the controller and will allow for easier testing of the
    message header.
    
    Also added a "body" method so more fully encapsulate the message
    processing code into the ImportJobRequest class.
    
    https://issues.umd.edu/browse/LIBHYDRA-410

---
+++ app/services/import_job_request.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Encapsulates a Plastron import job request message
+class ImportJobRequest
+  def initialize(job_id, import_job, validate_only)
+    @job_id = job_id
+    @job = import_job
+    @validate_only = validate_only
+  end
+
+  def headers # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    {
+      PlastronCommand: 'import',
+      PlastronJobId: @job_id,
+      'PlastronArg-model': @job.model,
+      'PlastronArg-name': @job.name,
+      'PlastronArg-on-behalf-of': @job.cas_user.cas_directory_id,
+      'PlastronArg-member-of': @job.collection,
+      'PlastronArg-timestamp': @job.timestamp
+    }.tap do |headers|
+      headers['PlastronArg-access'] = "<#{access}>" if @job.access.present?
+      headers['PlastronArg-validate-only'] = 'True' if @validate_only
+      headers['PlastronArg-binaries-location'] = @job.binaries_location if @job.binaries_location.present?
+    end
+  end
+
+  def body
+    @job.metadata_file.download
+  end
+end
