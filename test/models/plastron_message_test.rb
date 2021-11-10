783238252aa6fface52e3a202467a1c4014a3553
David P. Steelman <dsteelma@umd.edu>
2021-11-10 15:17:58 -0500

    LIBHYDRA-465. Cleaned up PlastronMessageTest
    Moved STOMP message creation into separate methods to clarify tests.
    
    https://issues.umd.edu/browse/LIBHYDRA-465

--- test/models/plastron_message_test.rb
+++ test/models/plastron_message_test.rb
@@ -4,50 +4,68 @@ require 'test_helper'
 
 class PlastronMessageTest < ActiveSupport::TestCase
   test 'parse_errors handles message with no errors' do
-    id = 'http://fcrepo-local:8080/fcrepo/rest/dc/2021/2/5f/e6/54/fb/5fe654fb-5fd7-4634-8be9-a73dd51bee80'
+    resource_id = 'http://fcrepo-local:8080/fcrepo/rest/dc/2021/2/5f/e6/54/fb/5fe654fb-5fd7-4634-8be9-a73dd51bee80'
+    stomp_message = create_stomp_message_with_no_errors(resource_id)
+
+    response = PlastronMessage.new(stomp_message)
+    errors = response.parse_errors(resource_id)
+
+    assert_equal [], errors
+  end
+
+  test 'parse_errors handles messsage with single validation error' do
+    resource_id = 'http://fcrepo-local:8080/fcrepo/rest/dc/2021/2/5f/e6/54/fb/5fe654fb-5fd7-4634-8be9-a73dd51bee80'
+    stomp_message = create_stomp_message_with_validation_error(resource_id)
+
+    response = PlastronMessage.new(stomp_message)
+    errors = response.parse_errors(resource_id)
+
+    assert_equal [{ name: 'title', status: 'failed', rule: 'required', expected: 'True' }], errors
+  end
+
+  test 'parse_errors handles messsage with single "other" error' do
+    resource_id = 'http://fcrepo-local:8080/fcrepo/rest/dc/2021/2/5f/e6/54/fb/5fe654fb-5fd7-4634-8be9-a73dd51bee80'
+    stomp_message = create_stomp_message_with_other_error(resource_id)
+
+    response = PlastronMessage.new(stomp_message)
+    errors = response.parse_errors(resource_id)
+
+    assert_equal [{ error: 'Some other error' }], errors
+  end
+
+  def create_stomp_message_with_no_errors(resource_id)
     stomp_message = Stomp::Message.new('')
     stomp_message.headers = {
       PlastronJobId: 'SYNCHRONOUS-435eba11-3287-4f98-9225-afe50dfcc685',
       PlastronJobState: 'update_complete'
     }
     stomp_message.body =
-      "{\"type\": \"update_complete\", \"stats\": {\"updated\": [\"#{id}\"], \"invalid\": {}, \"errors\": {}}}"
+      "{\"type\": \"update_complete\", \"stats\": {\"updated\": [\"#{resource_id}\"], \"invalid\": {}, \"errors\": {}}}"
 
-    response = PlastronMessage.new(stomp_message)
-    errors = response.parse_errors(id)
-
-    assert_equal [], errors
+    stomp_message
   end
 
-  test 'parse_errors handles messsage with single validation error' do
-    id = 'http://fcrepo-local:8080/fcrepo/rest/dc/2021/2/5f/e6/54/fb/5fe654fb-5fd7-4634-8be9-a73dd51bee80'
+  def create_stomp_message_with_validation_error(resource_id)
     stomp_message = Stomp::Message.new('')
     stomp_message.headers = {
       PlastronJobId: 'SYNCHRONOUS-889ad310-f796-43f2-8608-be3b56907414',
       PlastronJobState: 'update_incomplete'
     }
     stomp_message.body =
-      "{\"type\": \"update_incomplete\", \"stats\": {\"updated\": [], \"invalid\": {\"#{id}\": [\"('title', 'failed', 'required', True)\"]}, \"errors\": {}}}"
+      "{\"type\": \"update_incomplete\", \"stats\": {\"updated\": [], \"invalid\": {\"#{resource_id}\": [\"('title', 'failed', 'required', True)\"]}, \"errors\": {}}}"
 
-    response = PlastronMessage.new(stomp_message)
-    errors = response.parse_errors(id)
-
-    assert_equal [{ name: 'title', status: 'failed', rule: 'required', expected: 'True' }], errors
+    stomp_message
   end
 
-  test 'parse_errors handles messsage with single "other" error' do
-    id = 'http://fcrepo-local:8080/fcrepo/rest/dc/2021/2/5f/e6/54/fb/5fe654fb-5fd7-4634-8be9-a73dd51bee80'
+  def create_stomp_message_with_other_error(resource_id)
     stomp_message = Stomp::Message.new('')
     stomp_message.headers = {
       PlastronJobId: 'SYNCHRONOUS-889ad310-f796-43f2-8608-be3b56907414',
       PlastronJobState: 'update_incomplete'
     }
     stomp_message.body =
-      "{\"type\": \"update_incomplete\", \"stats\": {\"updated\": [], \"invalid\": {}, \"errors\": {\"#{id}\": \"Some other error\"}}}"
+      "{\"type\": \"update_incomplete\", \"stats\": {\"updated\": [], \"invalid\": {}, \"errors\": {\"#{resource_id}\": \"Some other error\"}}}"
 
-    response = PlastronMessage.new(stomp_message)
-    errors = response.parse_errors(id)
-
-    assert_equal [{ error: 'Some other error' }], errors
+    stomp_message
   end
 end
