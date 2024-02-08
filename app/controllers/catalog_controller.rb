089594d08b0ce772b41974d448ef14ba0cc16171
Mohamed Abdul Rasheed <mohideen@umd.edu>
2024-02-08 18:02:46 -0500

    LIBHYDRA-526. Refactored the changes.
    - Moved the fields to above the members metadata.
    - Renamed Discoverablity to Visiblity
    
    https://umd-dit.atlassian.net/browse/LIBHYDRA-526

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -132,6 +132,8 @@ class CatalogController < ApplicationController
     # the only fields being displayed form Solr are those relating to structural,
     # administrative, and technical metadata.
     config.add_show_field 'pcdm_collection', label: 'Collection', helper_method: :collection_from_subquery
+    config.add_show_field 'publication_status', label: 'Publication Status'
+    config.add_show_field 'visibility', label: 'Visibility'
     config.add_show_field 'pcdm_member_of', label: 'Member Of', helper_method: :parent_from_subquery
     config.add_show_field 'pcdm_members', label: 'Members', helper_method: :members_from_subquery
     config.add_show_field 'pcdm_related_object_of', label: 'Related To', helper_method: :related_object_of_from_subquery
@@ -148,8 +150,6 @@ class CatalogController < ApplicationController
     config.add_show_field 'created', label: 'Created'
     config.add_show_field 'last_modified', label: 'Last Modified'
     config.add_show_field 'rdf_type', label: 'RDF Type', helper_method: :rdf_type_list
-    config.add_show_field 'publication_status', label: 'Publication Status'
-    config.add_show_field 'visibility', label: 'Discoverability'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
