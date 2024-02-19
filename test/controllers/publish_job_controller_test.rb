5f958e2a35325c7ca01c5d87f80536665dd134eb
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-02-19 17:44:43 -0500

    LIBHYDRA-546: First test for index page
    https://umd-dit.atlassian.net/browse/LIBHYDRA-546

--- test/controllers/publish_job_controller_test.rb
+++ test/controllers/publish_job_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
-class PublishJobControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class PublishJobControllerTest < ActionController::TestCase
+  setup do
+    @cas_user = cas_users(:test_admin)
+    mock_cas_login(@cas_user.cas_directory_id)
+  end
+
+test "Users should see all jobs on the index page" do
+    assert PublishJob.count.positive?, 'Test requires at least one publish job'
+
+    get :index
+    jobs = assigns(:jobs)
+    assert_equal PublishJob.count, jobs.count
+  end
 end
