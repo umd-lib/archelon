5695bd3450454fb44065b992c60a92c7458ebc35
Peter Eichman <peichman@umd.edu>
2024-03-07 09:30:51 -0500

    LIBHYDRA-557. Correct name of table in migration.
    https://umd-dit.atlassian.net/browse/LIBHYDRA-557

--- db/migrate/20240227194858_add_index_and_resume_publish_job_requests.rb
+++ db/migrate/20240227194858_add_index_and_resume_publish_job_requests.rb
@@ -1,7 +1,7 @@
 class AddIndexAndResumePublishJobRequests < ActiveRecord::Migration[5.2]
   def change
     add_reference :publish_job_requests, :publish_job, index: true
-    add_foreign_key :publish_job_requests, :publish_job
+    add_foreign_key :publish_job_requests, :publish_jobs
     add_column :publish_job_requests, :resume, :boolean
   end
 end
