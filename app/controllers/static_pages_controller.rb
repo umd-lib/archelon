9c4234e9351b53ca5af6c4dfa733e73cb428e33a
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-04-17 16:38:18 -0400

    LIBHYDRA-577: Updating about controller to ping Solr
    When visiting the about page, pinging solr so that when refreshing
    the user can be sure that Solr is back up again. Previously refreshing
    the page would have user's think the service was back up. Now a flash
    screen will appear on refresh's of the page.
    
    https://umd-dit.atlassian.net/browse/LIBHYDRA-577

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
-
 class StaticPagesController < ApplicationController
   def about
+    # A simple ping to SOLR to see if it's running
+    begin
+      uri = URI(ENV['SOLR_URL'])
+      Net::HTTP.get(uri)
+    rescue Errno::ECONNREFUSED => e
+      solr_connection_error(e)
+    rescue SocketError => e
+      solr_connection_error(e)
+    end
   end
+
+  private
+    def solr_connection_error(err)
+      Rails.logger.error(err.message)
+      flash[:error] = I18n.t(:solr_is_down)
+    end
 end
