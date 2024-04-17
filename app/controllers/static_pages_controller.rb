4d81c8bea6356f07ffbf05aa52b37b0da4664851
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-04-17 16:57:25 -0400

    LIBHYDRA-577: Rubocop fixes
    https://umd-dit.atlassian.net/browse/LIBHYDRA-577

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
+
 class StaticPagesController < ApplicationController
   def about
     # A simple ping to SOLR to see if it's running
-    begin
-      uri = URI(ENV['SOLR_URL'])
-      Net::HTTP.get(uri)
-    rescue Errno::ECONNREFUSED => e
-      solr_connection_error(e)
-    rescue SocketError => e
-      solr_connection_error(e)
-    end
+    uri = URI(ENV['SOLR_URL'])
+    Net::HTTP.get(uri)
+  rescue Errno::ECONNREFUSED => e
+    solr_connection_error(e)
+  rescue SocketError => e
+    solr_connection_error(e)
   end
 
   private
+
     def solr_connection_error(err)
       Rails.logger.error(err.message)
       flash[:error] = I18n.t(:solr_is_down)
