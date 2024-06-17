265ab283c5fa9076fff39ebcf97db31560b2d82c
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-06-17 17:13:23 -0400

    LIBFCREPO-1446: Rubocop fix
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1446

--- test/jobs/delete_old_exports_job_test.rb
+++ test/jobs/delete_old_exports_job_test.rb
@@ -7,7 +7,7 @@ class DeleteOldExportsJobTest < ActiveJob::TestCase
     all_jobs = ExportJob.all.count
     old_jobs = ExportJob.where('created_at > ? ', 30.days.ago).count
 
-    assert all_jobs > 0
+    assert all_jobs.positive?
     DeleteOldExportsJob.perform_now
     assert_equal ExportJob.all.count, all_jobs - old_jobs
   end
