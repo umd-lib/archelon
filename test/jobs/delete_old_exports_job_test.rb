0ff653dfd302f59f314bafb6f9d27c5578413d8c
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-06-17 17:04:12 -0400

    LIBFCREPO-1446: Creating cronjob to delete old export jobs
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1446

---
+++ test/jobs/delete_old_exports_job_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class DeleteOldExportsJobTest < ActiveJob::TestCase
+  test 'deletes ExportJobs older than 30 days' do
+    all_jobs = ExportJob.all.count
+    old_jobs = ExportJob.where('created_at > ? ', 30.days.ago).count
+
+    assert all_jobs > 0
+    DeleteOldExportsJob.perform_now
+    assert_equal ExportJob.all.count, all_jobs - old_jobs
+  end
+end
