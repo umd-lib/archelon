e80c12d454ae10d5573014b151f054d7a2c67705
David P. Steelman <dsteelma@umd.edu>
2024-07-10 12:17:24 -0400

    LIBFCREPO-1471. Generated the Blacklight installation:
    Generated the Blacklight installation by running the following command:
    
    ```
    $ rails generate blacklight:install --skip-solr --bootstrap-version=5
    ```
    
    Used `--skip-solr` because we already have a Solr index
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,4 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
+import Blacklight from "blacklight-frontend";
+import githubAutoCompleteElement from "@github/auto-complete-element";
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  mount Blacklight::Engine => '/'
+  root to: "catalog#index"
+  concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
