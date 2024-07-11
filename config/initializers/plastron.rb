18e47bd01bc9ccdefa1e36b9af9aea45bddb0ca6
David P. Steelman <dsteelma@umd.edu>
2024-07-11 11:29:19 -0400

    LIBFCREPO-1471. Added PublicKey functionality
    Added Public Key functionality.
    
    See:
    
    * https://umd-dit.atlassian.net/browse/LIBHYDRA-305
    * https://umd-dit.atlassian.net/browse/LIBHYDRA-306
    * https://umd-dit.atlassian.net/browse/LIBHYDRA-307
    
    This also corrected a "TOFIX" on "cas_users_controller_test.rb", because
    the PublicKey model is now available.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- config/initializers/plastron.rb
+++ config/initializers/plastron.rb
@@ -1,5 +1,2 @@
 PLASTRON_REST_BASE_URL=ENV['PLASTRON_REST_BASE_URL']
 PLASTRON_PUBLIC_KEY = ENV['PLASTRON_PUBLIC_KEY']
-
-# register JSON Problem Details (RFC 9457) to parse as standard JSON
-HTTP::MimeType.register_adapter 'application/problem+json', HTTP::MimeType::JSON
