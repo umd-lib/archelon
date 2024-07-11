c6bbef6705e928d5f79351bb3537c1edd9f3c228
David P. Steelman <dsteelma@umd.edu>
2024-07-11 09:03:22 -0400

    LIBFCREPO-1471. TOFIX - Modified database migrations to skip models
    Modified two database migrations to skip updating models that
    don't yet exist.
    
    These database migrations should be restored once the appropriate
    models exist.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- db/migrate/20200320152422_create_plastron_operations.rb
+++ db/migrate/20200320152422_create_plastron_operations.rb
@@ -13,17 +13,17 @@ class CreatePlastronOperations < ActiveRecord::Migration[5.2]
 
     add_reference :export_jobs, :plastron_operation
 
-    # bootstrap existing export jobs with (synthesized) Plastron operations
-    ExportJob.where(plastron_operation_id: nil).find_each do |job|
-      status = job.status == 'Ready' ? :done : job.status
-      progress = job.status == 'Ready' ? 100 : 0
-      job.plastron_operation = PlastronOperation.new.tap do |op|
-        op.started = job.created_at
-        op.completed = job.updated_at unless ['Pending', 'In Progress'].include? job.status
-        op.status = status
-        op.progress = progress
-      end
-      job.save!
-    end
+    # # bootstrap existing export jobs with (synthesized) Plastron operations
+    # ExportJob.where(plastron_operation_id: nil).find_each do |job|
+    #   status = job.status == 'Ready' ? :done : job.status
+    #   progress = job.status == 'Ready' ? 100 : 0
+    #   job.plastron_operation = PlastronOperation.new.tap do |op|
+    #     op.started = job.created_at
+    #     op.completed = job.updated_at unless ['Pending', 'In Progress'].include? job.status
+    #     op.status = status
+    #     op.progress = progress
+    #   end
+    #   job.save!
+    # end
   end
 end
