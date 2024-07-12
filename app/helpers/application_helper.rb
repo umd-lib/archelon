5d743219b616a956734c238a2dfbd93e24f27bae
David P. Steelman <dsteelma@umd.edu>
2024-07-12 08:36:00 -0400

    LIBFCREPO-1471. Removed useless assignment in "from_subquery"
    Ruby LSP pointed out that the first "document_hash" assignment in
    the "from_subquery" method was useless, because of the assignment on
    the next line.
    
    Changed to simply returning the value of
    `args[:document][subquery_field]['docs']`
    because that was the simplest way to express what is happening.
    
    Note that in legacy Archelon, the code for this method was
    
    ```
      def from_subquery(subquery_field, args)
        args[:document][args[:field]] = args[:document][subquery_field]['docs']
      end
    ```
    
    but the reassignment to `args[:document][args[:field]]` is no longer
    possible, and doesn't seem to have any effect in any case.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
   def from_subquery(subquery_field, args)
     # UMD Blacklight 8 fix
     # see https://github.com/projectblacklight/blacklight/commit/dcfc74aebd4446aac85b2c2f7d10cd2c6ff8fef3
-    document_hash = args[:document].to_h
-    document_hash = args[:document][subquery_field]['docs']
+    # Can no longer assign to args[:document], so just return the value
+    args[:document][subquery_field]['docs']
     # End UMD Blacklight 8 fix
   end
 
