23c6258337e4fea01bb10ff8e24bdfba33927e7e
David P. Steelman <dsteelma@umd.edu>
2024-07-16 14:19:49 -0400

    LIBFCREPO-1471. Added React component loading to "application.js"
    Updated the "application.js" file to load the React JSX components
    from the "app/javascript/components" directory (or subdirectories).
    
    This relies on the “esbuild-plugin-import-glob” plugin to allow use of
    `import components from "./components/**/*.jsx"` to find all the React
    JSX components in the “app/javascript/components/” directory
    (and subdirectories) and then uses the “ReachRailsUJS” (from the
    “react-rails” gem) so that the components are usable in ERB templates
    using `<%= react_component(...) %>` syntax.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import * as bootstrap from "bootstrap"
 import Blacklight from "blacklight-frontend";
 import githubAutoCompleteElement from "@github/auto-complete-element";
+
+// Load in React components
+import components from "./components/**/*.jsx"
+
+let componentsContext = {}
+components.forEach((component) => {
+  componentsContext[component.default.name] = component.default
+})
+
+const ReactRailsUJS = require("react_ujs")
+
+ReactRailsUJS.getConstructor = (name) => {
+  return componentsContext[name]
+}
