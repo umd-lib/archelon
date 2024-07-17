796abb038a2be48190af9a4cf38a6d1826bad768
David P. Steelman <dsteelma@umd.edu>
2024-07-17 07:33:28 -0400

    LIBFCREPO-1471. Add "VocabularyService" functionality
    The React JSX components used in the "react_components" endpoints use
    the VocabularyService to populate some fields, so adding in the
    "VocbularyService" functionality.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

---
+++ app/services/vocabulary_term.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Data class encapsulating a single vocabulary term
+class VocabularyTerm
+  attr_reader :identifier # The simple human-readable identifier for the term, typically "dc:identifier"
+  attr_reader :uri        # Tne URI for the term (typically http://vocab.lib.umd.edu/<identifier>#)
+  attr_reader :label      # The "rdfs:label" for the term (may be nil)
+  attr_reader :same_as    # The "'owl:sameAs" id for the term (may be nil)
+
+  # Creates a VocabularyTerm with the given parameters
+  def initialize(identifier:, uri:, label:, same_as:)
+    @identifier = identifier
+    @uri = uri
+    @label = label
+    @same_as = same_as
+  end
+end
