176bb7290e3fd1b9597ade71cb04055482ec80a2
David P. Steelman <dsteelma@umd.edu>
2024-07-29 13:50:42 -0400

    LIBFCREPO-1471. Copied "download_urls_controller_test" from legacy
    Copied the "download_urls_controller_test.rb" controller test "as-is"
    from legacy Archelon.
    
    Added the "enabled" attribute to the "ransackable_attributes" parameter
    in "download_url.rb" to enable the tests to pass.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

---
+++ test/controllers/download_urls_controller_test.rb
@@ -0,0 +1,183 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class DownloadUrlsControllerTest < ActionController::TestCase
+  setup do
+    @download_url = download_urls(:one)
+    @cas_user = cas_users(:test_admin)
+    mock_cas_login(@cas_user.cas_directory_id)
+  end
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:download_urls)
+  end
+
+  test 'should add creator on create' do
+    stub_find_solr_document do
+      assert_difference('DownloadUrl.count') do
+        post :create_download_url, params: {
+          download_url: {
+            accessed_at: @download_url.accessed_at,
+            download_completed_at: @download_url.download_completed_at,
+            enabled: @download_url.enabled, mime_type: @download_url.mime_type,
+            notes: @download_url.notes, request_ip: @download_url.request_ip,
+            request_user_agent: @download_url.request_user_agent,
+            title: @download_url.title,
+            url: @download_url.url
+          }
+        }
+      end
+    end
+    download_url = DownloadUrl.last
+    assert_equal session[:cas_user], download_url.creator
+  end
+
+  test 'should add the "real" user as creator while impersonating' do
+    user_one = cas_users(:one)
+    stub_find_solr_document do
+      assert_difference('DownloadUrl.count') do
+        impersonate_as_user(user_one) do
+          assert_equal session[:cas_user], user_one.cas_directory_id
+          post :create_download_url, params: {
+            download_url: {
+              accessed_at: @download_url.accessed_at,
+              download_completed_at: @download_url.download_completed_at,
+              enabled: @download_url.enabled, mime_type: @download_url.mime_type,
+              notes: @download_url.notes, request_ip: @download_url.request_ip,
+              request_user_agent: @download_url.request_user_agent,
+              title: @download_url.title,
+              url: @download_url.url
+            }
+          }
+        end
+      end
+    end
+    download_url = DownloadUrl.last
+    assert_equal session[:cas_user], @cas_user.cas_directory_id
+    assert_equal session[:cas_user], download_url.creator
+    assert_not_equal user_one.cas_directory_id, download_url.creator
+  end
+
+  test 'should not be able to set token or creator on create' do
+    token_to_try = '12345'
+    creator_to_try = 'one'
+    stub_find_solr_document do
+      assert_difference('DownloadUrl.count') do
+        post :create_download_url, params: {
+          download_url: {
+            accessed_at: @download_url.accessed_at,
+            download_completed_at: @download_url.download_completed_at,
+            enabled: @download_url.enabled, mime_type: @download_url.mime_type,
+            notes: @download_url.notes, request_ip: @download_url.request_ip,
+            request_user_agent: @download_url.request_user_agent,
+            title: @download_url.title,
+            url: @download_url.url, token: token_to_try, creator: creator_to_try
+          }
+        }
+      end
+    end
+    download_url = DownloadUrl.last
+    assert_not_equal token_to_try, download_url.token
+    assert_not_equal creator_to_try, download_url.creator
+  end
+
+  test 'should require a note on create' do
+    stub_find_solr_document do
+      assert_no_difference('DownloadUrl.count') do
+        post :create_download_url, params: {
+          download_url: {
+            accessed_at: @download_url.accessed_at,
+            download_completed_at: @download_url.download_completed_at,
+            enabled: @download_url.enabled, mime_type: @download_url.mime_type,
+            notes: nil, request_ip: @download_url.request_ip,
+            request_user_agent: @download_url.request_user_agent,
+            title: @download_url.title,
+            url: @download_url.url
+          }
+        }
+      end
+    end
+  end
+
+  test 'should show download_url' do
+    # Set a default RETRIEVE_BASE_URL if none is set, so that Jenkins can
+    # run the test.
+    ENV['RETRIEVE_BASE_URL'] = 'http://example.com/' if ENV['RETRIEVE_BASE_URL'].nil?
+    get :show, params: { id: @download_url }
+    assert_response :success
+  end
+
+  test 'generate_download_url should respond with 404 if document cannot be found' do
+    @controller.stub(:find_solr_document, nil) do
+      get :generate_download_url, params: { document_url: 'document does not exist' }
+      assert_response :not_found
+    end
+  end
+
+  test 'create_download_url should respond with 404 if document cannot be found' do
+    @controller.stub(:find_solr_document, nil) do
+      get :create_download_url, params: { document_url: 'document does not exist' }
+      assert_response :not_found
+    end
+  end
+
+  test 'show_download_url should assign the retrieve url' do
+    # Assign a dummy RETRIEVE_BASE_URL so test can run without a .env file
+    ENV['RETRIEVE_BASE_URL'] = 'https://example.com/'
+    get :show_download_url, params: { token: @download_url.token }
+    retrieve_base_url = ENV['RETRIEVE_BASE_URL']
+    assert_not assigns(:download_url).nil?
+    assert_equal retrieve_base_url + @download_url.token, assigns(:download_url).retrieve_url
+  end
+
+  test 'disable should disable an enabled download_url' do
+    assert @download_url.enabled?
+
+    put :disable, params: { token: @download_url.token }
+
+    @download_url.reload
+    assert_not @download_url.enabled?
+  end
+
+  test 'index should allow filtering by "enabled" status' do
+    get :index, params: { rq: { enabled_eq: 1 } }
+
+    download_urls = assigns(:download_urls)
+    assert download_urls.count.positive?
+    assert download_urls.all?(&:enabled?)
+  end
+
+  test 'index should allow filtering by creator' do
+    creator = @download_url.creator
+    get :index, params: { rq: { creator_eq: creator } }
+
+    download_urls = assigns(:download_urls)
+    assert download_urls.count.positive?
+    assert(download_urls.all?) { |d| d.creator == creator }
+  end
+
+  private
+
+    # Stubs the DownloadUrlsController.find_solr_document call, so that it
+    # won't actually make a network call. Returns a sample SolrDocument
+    #
+    # Usage:
+    #
+    # stub_find_solr_document do
+    #   <Code that calls "find_solr_document">
+    # end
+    def stub_find_solr_document
+      stub_response = SolrDocument.new(
+        id: 'http://www.example.com',
+        mime_type: 'image/jp2',
+        display_title: 'Example'
+      )
+
+      @controller.stub :find_solr_document, stub_response do
+        yield
+      end
+    end
+end
