b710599094d928f184ebade90fb5859761223376
David P. Steelman <dsteelma@umd.edu>
2024-07-30 12:05:02 -0400

    LIBFCREPO-1471. Added "Publish"/"Unpublish" to Bookmarks index page
    Modified the "bookmarks_controller.rb" to add "Publish"/"Unpublish"
    buttons to the Bookmarks index page.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,5 +5,7 @@ class BookmarksController < CatalogController
 
   # UMD Customization
   blacklight_config.add_show_tools_partial(:export, path: :new_export_job_url, modal: false)
+  blacklight_config.add_show_tools_partial(:publish_job, path: :new_publish_job_url, modal: false, label: 'Publish')
+  blacklight_config.add_show_tools_partial(:unpublish_job, path: :new_unpublish_job_url, modal: false, label: 'Unpublish')
   # End UMD Customization
 end
