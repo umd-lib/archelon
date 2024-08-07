6581ab0882c1a0145b59f1687fda695ec123a788
David P. Steelman <dsteelma@umd.edu>
2024-08-07 09:26:34 -0400

    LIBFCREPO-1471. Restored skipped tests by fixing stub_solr_request
    Fixed tests by updating the "stub_solr_request" method in each test
    to reflect changes in Blacklight 8, i.e., “search_results” method is now
    in “Blacklight::SearchService”, instead of “CatalogController”, and only
    returns a “Blacklight::Solr::Response” instead of a two-part array
    consisting of a Solr response and an array.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- test/integration/cas_authentication_test.rb
+++ test/integration/cas_authentication_test.rb
@@ -10,13 +10,14 @@ class CasAuthenticationTest < ActionDispatch::IntegrationTest
   # Stubs an empty Solr response, for use when actually retrieving a
   # web page in the test.
   def stub_solr_response
+    # UMD Blacklight 8 Fix
     solr_response = double(Blacklight::Solr::Response)
     expect(solr_response).to receive(:aggregations).at_least(:once).and_return({})
-    expect_any_instance_of(CatalogController).to receive(:search_results).and_return([solr_response, []])
+    expect_any_instance_of(Blacklight::SearchService).to receive(:search_results).and_return(solr_response)
+    # End UMD Blacklight 8 Fix
   end
 
   test 'valid logins should have session and cookie values set' do
-    skip("TOFIX: Making unexpected web call")
     stub_solr_response
     # Using mock_cas_login_for_integration_tests, because this is an
     # existing test fixtures user
