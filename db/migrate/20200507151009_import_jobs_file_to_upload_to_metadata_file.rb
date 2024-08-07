64f7860fdb21a79c5143621b0ca89fec40d6199a
David P. Steelman <dsteelma@umd.edu>
2024-08-07 13:17:05 -0400

    LIBFCREPO-1471. Restored database migration be same as legacy Archelon
    The change in this database migration was originally made because
    when doing the initial file copy for the upgrade, the "ImportJob" class
    was not included. Now that the "ImportJob" class is available,
    restoring this migration to its original content, matching legacy
    Archelon.
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1471

--- db/migrate/20200507151009_import_jobs_file_to_upload_to_metadata_file.rb
+++ db/migrate/20200507151009_import_jobs_file_to_upload_to_metadata_file.rb
@@ -1,5 +1,5 @@
 class ImportJobsFileToUploadToMetadataFile < ActiveRecord::Migration[5.2]
   def change
-    # ImportJob.connection.execute("UPDATE active_storage_attachments SET name = 'metadata_file' WHERE name = 'file_to_upload'")
+    ImportJob.connection.execute("UPDATE active_storage_attachments SET name = 'metadata_file' WHERE name = 'file_to_upload'")
   end
 end
