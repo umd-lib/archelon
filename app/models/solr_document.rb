26a6d2a8afb0f8bc9c31d224f38f919359b39913
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2025-03-28 15:59:30 -0400

    LIBFCREPO-1588: Configurations for new solr core
    New fields to access, seperated by item, page, and file level content
    
    Blacklight also added accessors to manipulate the data before
    rendering it, within solr_document.rb, referenced by an accessor
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1588

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -12,4 +12,68 @@ class SolrDocument
   # and Blacklight::Document::SemanticFields#to_semantic_values
   # Recommendation: Use field names from Dublin Core
   use_extension(Blacklight::Document::DublinCore)
+
+  def add_language_badge
+    return unless has? 'item__title__display'
+
+    Array(fetch('item__title__display')).map do |title|
+      if title.starts_with? '['
+        language = title.split(']')[0][2...]
+        title = title.split(']')[1]
+
+        title.html_safe + " <span class=\"badge text-bg-secondary\">#{language}</span>".html_safe
+      else
+        title.html_safe
+      end
+    end
+  end
+
+  def archival_collection_anchor
+    return unless has? 'item__archival_collection__uri'
+    add_anchor_tag(fetch('item__archival_collection__uri'), fetch('item__archival_collection__label__txt'))
+  end
+
+  def handle_anchor
+    return unless has? 'handle__id'
+    add_anchor_tag(fetch('handle_proxied__uri'), fetch('handle__id'))
+  end
+
+  def members_anchor
+    return unless has? 'page_uri_sequence__uris'
+    labels = fetch('page_label_sequence__txts')
+    uris = fetch('page_uri_sequence__uris')
+
+    paired_uris = uris.zip(labels)
+
+    paired_uris.map do |uri, label|
+      add_anchor_tag(uri, label)
+    end
+
+  end
+
+  def member_of_anchor
+    return unless has? 'item__member_of__uri'
+    add_anchor_tag(fetch('item__member_of__uri'), fetch('item__member_of__uri'))
+  end
+
+  def rights_anchor
+    return unless has? 'item__rights__uri'
+    add_anchor_tag(fetch('item__rights__uri'), fetch('item__rights__label__txt'))
+  end
+
+  def terms_anchor
+    return unless has? 'item__terms_of_use__uri'
+    add_anchor_tag(fetch('item__terms_of_use__uri'), fetch('item__terms_of_use__label__txt'))
+  end
+
+  def object_type_anchor
+    return unless has? 'item__object_type__uri'
+    add_anchor_tag(fetch('item__object_type__uri'), fetch('item__object_type__uri'))
+  end
+
+  private
+
+    def add_anchor_tag(uri, label)
+      "<a href=#{uri}> #{label} </a>".html_safe
+    end
 end
