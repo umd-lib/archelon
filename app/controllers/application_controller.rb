8043e0bbff62483ecdf40b303ef205a0c28fb509
David P. Steelman <dsteelma@umd.edu>
2025-08-13 11:30:59 -0400

    LIBFCREPO-1525. Rubocop fixes - Rails/RootPublicPath
    Fixed "Rails/RootPublicPath" Rubocop warnings.
    https://docs.rubocop.org/rubocop-rails/cops_rails.html#railsrootpublicpath
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1525

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   rescue_from CanCan::AccessDenied do
-    render file: Rails.root.join('public/403.html'), status: :forbidden, layout: false
+    render file: Rails.public_path.join('403.html'), status: :forbidden, layout: false
   end
 
   before_action :authenticate
@@ -27,15 +27,15 @@ class ApplicationController < ActionController::Base
 
   # Causes a "404 - Not Found" error page to be displayed.
   def not_found
-    render file: Rails.root.join('public/404.html'), status: :not_found, layout: false
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
 
   def forbidden
-    render file: Rails.root.join('public/403.html'), status: :forbidden, layout: false
+    render file: Rails.public_path.join('403.html'), status: :forbidden, layout: false
   end
 
   def bad_request
-    render file: Rails.root.join('public/500.html'), status: :bad_request, layout: false
+    render file: Rails.public_path.join('500.html'), status: :bad_request, layout: false
   end
 
   def impersonating?
