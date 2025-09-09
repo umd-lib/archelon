dad58d5f73844096032790b248abcd1291b4a840
Peter Eichman <peichman@umd.edu>
2025-09-09 17:00:31 -0400

    LIBFCREPO-1553. Disable origin checking for ActionCable.
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1553

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # disable this check since getting a reliable list of origins has proved difficult
+  config.action_cable.disable_request_forgery_protection = true
 end
