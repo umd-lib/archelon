3d46abd2305d27986573347845a7fc7c39c39294
Peter Eichman <peichman@umd.edu>
2026-02-01 20:01:31 -0500

    Bumped version to 2.0.1

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Archelon
-  VERSION = '2.0.0'
+  VERSION = '2.0.1'
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
